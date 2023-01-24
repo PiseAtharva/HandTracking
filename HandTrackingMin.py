93bd1adb92c3fbdfd9c31b0c757f9d1e33cc93c5
Atharva Pise <atharvapise19@gmail.com>
2023-01-24 20:54:55 +0530

    computing fps

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -8,6 +8,9 @@ mpHands = mp.solutions.hands
 hands = mpHands.Hands()             # working with default params in this case
 mpDraw = mp.solutions.drawing_utils
 
+pTime = 0
+cTime = 0
+
 while True:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -18,6 +21,12 @@ while True:
     if results.multi_hand_landmarks:
         for handLMS in results.multi_hand_landmarks:
             mpDraw.draw_landmarks(img, handLMS, mpHands.HAND_CONNECTIONS)
+
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 0), 3)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
