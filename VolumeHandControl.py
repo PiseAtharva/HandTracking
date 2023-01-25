313c59bfebbf708178f838b3432fd67b78399048
Atharva Pise <atharvapise19@gmail.com>
2023-01-25 11:55:31 +0530

    Configuring HandtrackingModule for Hand Tracking

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -1,11 +1,26 @@
 import cv2
 import time
 import numpy as np
+import HandTrackingModule as htm
 
+#######################
+wCam, hCam = 640, 480
+#######################
 cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+pTime = 0
 
+detector = htm.handDetector(detectionConf=0.7)
 while True:
     success, img = cap.read()
+    detector.findHands(img)
+
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, f'FPS: {int(fps)}', (10, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
 
     cv2.imshow("Img", img)
     key = cv2.waitKey(1)
