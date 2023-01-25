8d66e3cf385f6947bd74d3d3b648601e975446a4
Atharva Pise <atharvapise19@gmail.com>
2023-01-25 12:18:08 +0530

    Locating Tips and measuring length between them

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -2,6 +2,7 @@ import cv2
 import time
 import numpy as np
 import HandTrackingModule as htm
+import math
 
 #######################
 wCam, hCam = 640, 480
@@ -15,6 +16,22 @@ detector = htm.handDetector(detectionConf=0.7)
 while True:
     success, img = cap.read()
     detector.findHands(img)
+    lmList = detector.findPosition(img, draw=False)
+    if len(lmList) != 0:
+        #print(lmList[4], lmList[8])     #printing tip of thumb and index cordinates on img
+
+        x1, y1 = lmList[4][1], lmList[4][2]
+        x2, y2 = lmList[8][1], lmList[8][2]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        cv2.circle(img, (x1, y1), 15, (255, 0 , 255), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 15, (255, 0 , 255), cv2.FILLED)
+        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+        cv2.circle(img, (cx, cy), 15, (255, 0 , 255), cv2.FILLED)
+        length = math.hypot(x2 - x1, y2 - y1)
+        print(length)
+        if length < 30:
+            cv2.circle(img, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
 
     cTime = time.time()
     fps = 1/(cTime - pTime)
