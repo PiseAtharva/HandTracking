4c67c14432de028adfc7c85f90746799c6906163
Atharva Pise <atharvapise19@gmail.com>
2023-01-25 12:56:37 +0530

    Moderating Volume via length parameter

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -3,6 +3,9 @@ import time
 import numpy as np
 import HandTrackingModule as htm
 import math
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
 #######################
 wCam, hCam = 640, 480
@@ -13,6 +16,21 @@ cap.set(4, hCam)
 pTime = 0
 
 detector = htm.handDetector(detectionConf=0.7)
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+# volume.GetMute()
+# volume.GetMasterVolumeLevel()
+volRange = volume.GetVolumeRange()
+# volume.SetMasterVolumeLevel(-20.0, None)
+minVol = volRange[0]
+maxVol = volRange[1]
+vol = 0
+volBar = 400
+volPercent = 0
+
 while True:
     success, img = cap.read()
     detector.findHands(img)
@@ -29,15 +47,29 @@ while True:
         cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
         cv2.circle(img, (cx, cy), 15, (255, 0 , 255), cv2.FILLED)
         length = math.hypot(x2 - x1, y2 - y1)
-        print(length)
+
+        # print(length)
         if length < 30:
             cv2.circle(img, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
+
+    #Hand Range was 30 to 150
+    #Volume Range of -65 to 0
+
+        vol = np.interp(length, [30, 150], [minVol, maxVol])
+        volBar = np.interp(length, [30, 150], [400, 150])
+        print(int(length), vol)
+        volPercent = np.interp(length, [30, 150], [0, 100])
+        volume.SetMasterVolumeLevel(vol, None)
+
+    cv2.rectangle(img, (50, 150), (85, 400), (0, 255, 0), 3)
+    cv2.rectangle(img, (50, int(volBar)), (85, 400), (255, 0, 0), cv2.FILLED)
 
     cTime = time.time()
     fps = 1/(cTime - pTime)
     pTime = cTime
 
     cv2.putText(img, f'FPS: {int(fps)}', (10, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+    cv2.putText(img, f'{int(volPercent)}%', (40, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 3)
 
     cv2.imshow("Img", img)
     key = cv2.waitKey(1)
